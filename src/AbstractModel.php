8aa572d9dd8a08a0327dfb53bb7c2192748ef72d
thedigital <guillaume.ferrand@gmail.com>
2015-01-21 14:02:19 +0100

    ajout application name sous forme de setter

--- src/AbstractModel.php
+++ src/AbstractModel.php
@@ -26,7 +26,7 @@ abstract class AbstractModel
     public function setApplicationName($application_name)
     {
         $this->db_connection->exec('SET application_name = "'.$application_name.'"');
-        $this->db_connection->exec('select pg_sleep(3)');
+        //$this->db_connection->exec('select pg_sleep(3)');
     }
 
     public function getConnection()
